1980abd695d7912cdc03148dc3801f6fea477f70
imkyk19 <imkyk19@gmail.com>
2023-10-27 12:49:33 +1300

    Nav bar scroll

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 
 //컴포넌트 사용을 하려면 const로 선언해줘야 함.
 function Nav() {
+
+  //상태 관리
+  const [show, setShow] = React.useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      //스크롤 위치 50 이하인 경우
+      if (window.scrollY > 50) {
+        setShow(true);
+        // 스크롤 위치 50 이상인 경우
+      }else{
+        setShow(false);
+      }
+    })
+  
+    //컴포넌트가 더 이상 사용되지 않을 때 호출 *꼭 필요. 에러방지.
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    }
+
+    /* 빈 배열 : 랜더링 후 컴포넌트 한 번 시작
+        스테이트가 들어가면 스테이트가 변할 때 마다 시작 */
+  }, [])
+  
+
+
   return (
-    <Navrapper>
+    //show 여부 가져오기.
+    <Navrapper show={show}>
       <Logo>
         <img
           //이미지 소스가 안 보일 경우 보여지는 글귀
@@ -24,7 +51,8 @@ const Navrapper = styled.nav`
   top : 0;
   left : 0;
   right : 0;
-  background-color : #090b13;
+  //props로 show 가져와서 true : 투명 / false : 검정색
+  background-color : ${props => props.show ? "#090b13" : "transparent" };
   display : flex;
   justify-content : space-between;
   align-items : center;
